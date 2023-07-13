d3c8113a01d946d6994ddb4cd378a64ff577f559
SamThompson57 <s_thomo@hotmail.co.uk>
2023-07-13 11:55:06 +0100

    searchbars styles next finish invitations to upload CV and start allowing content

--- src/Body.js
+++ src/Body.js
@@ -1,25 +1,45 @@
 import React from "react";
+import magGlass from "./components/magGlass.png"
+import locPin from "./components/locPin.png"
 
 const Body = () => {
     return (
         <div className="body">
-            <div>
-                <div className="searchContainer">
-                    <form className="searchBar">
+            <div>{/*This can probably be its own module*/}
+                <form className="searchContainer">
+                    <div className="searchBar">
                         <div className="searchField">
                             <label>What</label>
                             <input type="text" placeholder="Job title, keywords, or company"></input>
-                            <div>img</div>
+                            <div>
+                                <img className="searchImg" src={magGlass} alt=""></img>
+                            </div>
                         </div>
                         <div className="searchField">
                             <label>Where</label>
                             <input type="text" placeholder="city or postcode"></input>
-                            <div>img</div>
+                            <div>
+                                <img className="searchImg" src={locPin} alt=""></img>
+                            </div>
                         </div>
-                    </form>
-                </div>
+                        
+                    </div>
+                    <button className="searchField" id="submit">Find jobs</button>
+                </form>
             </div>
-            
+            <div className="content">
+                <div>
+                    <div className="invite">
+                        <div className="invLink">Upload your CV </div>
+                        <div id="dash">–</div>
+                        It only takes a few seconds
+                    </div>
+                    <div className="invite">
+                        <div className="invLink">Post a job on Inneed</div>
+                    </div>
+                </div>
+                
+            </div>
             
         </div>
     )
